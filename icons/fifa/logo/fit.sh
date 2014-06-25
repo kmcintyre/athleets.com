7f1f40b5dc093a902c0bf0914cc30308654986d2
Kevin McIntyre <kmcintyre@greenchargenet.com>
2014-06-25 08:51:49 -0700

    clean up

--- icons/fifa/logo/fit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for f in /home/kevin/scewpt/athleets/fifa/logo/*.svg
-do
-	echo `basename $f`	
-	rsvg-convert `basename $f` -h 15 -f svg -o 15_`basename $f`
-done
--- icons/mlb/logo/fit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in /home/kevin/scewpt/ahtleets/mlb/logo/*.svg
-do
-	echo `basename $f`	
-	rsvg-convert `basename $f` -h 20 -f svg -o 20_`basename $f`
-done
